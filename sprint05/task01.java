a8c6f06f97c1d2ada6682eab25827a2bfb930c93
Marta Pryshlyak <pryshlyak@ucu.edu.ua>
2021-08-27 17:37:34 +0300

    Create task01.java

---
+++ sprint05/task01.java
@@ -0,0 +1,16 @@
+class Operation{
+    public static  int squareRectangle (int a, int b) {
+        if((a<=0)||(b<=0)){
+            throw new IllegalArgumentException("both arguments should be more than zero");
+        }
+        return a * b;
+    }
+
+    public static int trySquareRectangle(int a, int b) {
+        try{
+            return squareRectangle(a, b);
+        } catch (IllegalArgumentException e){
+        return -1;
+        }
+    }
+}
