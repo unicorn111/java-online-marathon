159ebc66974417bb5e5e1cd7241b7a5dcae70cd9
Marta Pryshlyak <pryshlyak@ucu.edu.ua>
2021-08-27 17:30:37 +0300

    Update task02.java

--- sprint03/task02.java
+++ sprint03/task02.java
@@ -1,86 +1,24 @@
-class Pizza {
-    private String cheese;
-    private String meat;
-    private String seafood;
-    private String vegetable;
-    private String mushroom;
+class NameList {
+    private String[] names = {"Mike", "Emily", "Nick", "Patric", "Sara"};
 
-    private Pizza() {
+    public Iterator getIterator() {
+        return new Iterator();
     }
 
-    public String getCheese() {
-        return cheese;
-    }
-
-    public String getMeat() {
-        return meat;
-    }
-
-    public String getSeafood() {
-        return seafood;
-    }
-
-    public String getVegetable() {
-        return vegetable;
-    }
-
-    public String getMushroom() {
-        return mushroom;
-    }
-
-    public static PizzaBuilder base() {
-        return new PizzaBuilder();
-    }
-
-    public static class PizzaBuilder {
-        private Pizza pizza;
-
-        private PizzaBuilder() {
-            pizza = new Pizza();
+    public class Iterator{
+        private int counter = -1;
+        
+        private Iterator(){}
+        
+        public boolean hasNext(){
+            if((counter + 1) == names.length) {
+            	return false;
+            }
+            return true;
         }
-
-        private PizzaBuilder(Pizza pizza) {
-            this.pizza = pizza;
+        public String next(){
+        	counter +=1;
+            return names[counter];
         }
-
-        public PizzaBuilder addCheese(String cheese) {
-            pizza.cheese = cheese;
-            return this;
-        }
-
-        public PizzaBuilder addMeat(String meat) {
-            pizza.meat = meat;
-            return this;
-        }
-
-        public PizzaBuilder addSeafood(String seafood) {
-            pizza.seafood = seafood;
-            return this;
-        }
-
-        public PizzaBuilder addVegetable(String vegetable) {
-            pizza.vegetable = vegetable;
-            return this;
-        }
-
-        public PizzaBuilder addMushroom(String mushroom) {
-            pizza.mushroom = mushroom;
-            return this;
-        }
-
-        public Pizza build() {
-            return pizza;
-        }
-    }
-
-}
-
-class Oven {
-    public static Pizza cook() {
-        return Pizza.base()
-                .addMeat("meat")
-                .addVegetable("vegetable")
-                .addCheese("cheese")
-                .build();
     }
 }
